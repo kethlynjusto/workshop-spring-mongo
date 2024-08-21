cac47b75a8314d3541f14b83397b16177ab20aa2
Kethlyn Justo <kethdiniz06@gmail.com>
2024-08-21 10:55:59 -0300

    first commit

---
+++ workshop-spring-mongo/src/main/java/com/kethlyn/workshop_spring_mongo/WorkshopSpringMongoApplication.java
@@ -0,0 +1,13 @@
+package com.kethlyn.workshop_spring_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringMongoApplication.class, args);
+	}
+
+}

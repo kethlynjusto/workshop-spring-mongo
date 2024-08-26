a1dbf17eb2b5cca87c1af0df8b456fa7e5f096ff
Kethlyn Justo <kethdiniz06@gmail.com>
2024-08-26 16:10:55 -0300

    Criando Crud

--- workshop-spring-mongo/src/main/java/com/kethlyn/workshop_spring_mongo/resources/UserResource.java
+++ workshop-spring-mongo/src/main/java/com/kethlyn/workshop_spring_mongo/resources/UserResource.java
@@ -6,7 +6,9 @@ import com.kethlyn.workshop_spring_mongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,11 +27,32 @@ public class UserResource {
         return ResponseEntity.ok().body(listDTO);
     }
 
+
     @RequestMapping(value="/{id}", method = RequestMethod.GET)
     public ResponseEntity<UserDTO> findById(@PathVariable String id) {
         User obj = service.findbyId(id);
         return ResponseEntity.ok().body(new UserDTO(obj));
-
     }
 
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO) {
+        User obj = service.fromDTO(objDTO);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDTO, @PathVariable String id) {
+        User obj = service.fromDTO(objDTO);
+        obj.setId(id);
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- workshop-spring-mongo/src/main/java/com/kethlyn/workshop_spring_mongo/services/UserService.java
+++ workshop-spring-mongo/src/main/java/com/kethlyn/workshop_spring_mongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.kethlyn.workshop_spring_mongo.services;
 
 import com.kethlyn.workshop_spring_mongo.domain.User;
+import com.kethlyn.workshop_spring_mongo.dto.UserDTO;
 import com.kethlyn.workshop_spring_mongo.repository.UserRepository;
 import com.kethlyn.workshop_spring_mongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,4 +25,32 @@ public class UserService {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
     }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public void delete(String id) {
+        findbyId(id);
+        repo.deleteById(id);
+    }
+
+    public User update(User obj) {
+        User newObj = findbyId(obj.getId());
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+
+    private void updateData(User newObj, User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+    }
+
+    public User fromDTO(UserDTO objDTO){
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+    }
+
+
+
+
 }
